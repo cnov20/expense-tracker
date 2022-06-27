cff0597d32c1b1fb50f16d026b98d6d23bcffd3e
Chris Novelli <chrisnovelli0@gmail.com>
2022-06-27 16:01:28 -0400

    Created Expense Item component — added styling and added to ExpensesList component/re-factored

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import React from 'react';
+import ExpenseItem from '../ExpenseItem';
 
 function renderExpenseItem(itemData) {
 
   return (
-    <Text>{itemData.item.description}</Text>
+    <ExpenseItem {...itemData.item}/>
   )
 
 }
