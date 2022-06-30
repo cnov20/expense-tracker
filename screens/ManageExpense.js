55fce8a1866f72467cc566ef0662a81bd08a3996
Chris Novelli <chrisnovelli0@gmail.com>
2022-06-30 12:33:32 -0400

    Added framework for a Delete feature to Manage Expense Component/Screen — Icon/Button/function definition

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
 import react, { useLayoutEffect, useEffect } from 'react';
+import IconButton from '../components/UI/IconButton';
+import { GlobalStyles } from '../constants/styles';
 
 const ManageExpense = ({ route, navigation }) => {
 
@@ -15,14 +17,31 @@ const ManageExpense = ({ route, navigation }) => {
   // useEffect(() => {
     
   // }, [])
+
+  function deleteExpenseHandler() { };
   
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <View style={styles.deleteContainer}>
+      {isEditing && <IconButton icon={"trash"} color={GlobalStyles.colors.error500} size={36} onPress={deleteExpenseHandler} />}
+      </View>
     </View>
   )
 }
 
 export default ManageExpense;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800
+  }
+  deleteContainer: {
+    marginTop: 16,
+    padding: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center'
+  }
+})
